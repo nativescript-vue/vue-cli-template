04d3cae290b971c913a6f6abf90c3805f3ecdef9
Pascal Martineau <pascal@lewebsimple.ca>
2018-02-19 21:53:40 -0500

    Configure babel-loader with babel-preset-env

--- template/webpack.config.js
+++ template/webpack.config.js
@@ -10,6 +10,21 @@ const config = platform => {
       path: path.resolve(__dirname, './dist/app'),
       filename: `app.bundle.${platform}.js`,
     },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['babel-preset-env'],
+              cacheDirectory: true,
+            },
+          },
+        },
+      ],
+    },
     resolve: {
       modules: [
         'node_modules/tns-core-modules',
