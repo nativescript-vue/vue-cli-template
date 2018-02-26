837b4b23f7c9ac0a1c100a73197ba369f273b0d0
rigor789 <rigor789@gmail.com>
2018-02-26 12:55:01 +0100

    Add sample app content

---
+++ template/src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+    <StackLayout class="HelloWorld">
+        <Label text="This is a hello world component, tap the button if you dare!" textWrap="true"/>
+        <Button @tap="surprise = !surprise" text="Tap me!"/>
+
+        <Image v-if="surprise" src="https://art.nativescript-vue.org/NativeScript-Vue.png"/>
+    </StackLayout>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        surprise: false,
+      }
+    },
+  }
+</script>
+
+<style scoped>
+    .HelloWorld {
+        margin: 20;
+    }
+
+    Label {
+        color: red;
+    }
+
+    Button {
+        background-color: #42b883;
+        color: #35495e;
+        padding: 20;
+        font-size: 20;
+        margin: 20 0;
+    }
+</style>
