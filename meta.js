58e6e954ab0dcb70e0eb7b516c8e9df4a6da5f07
Pascal Martineau <pascal@lewebsimple.ca>
2018-03-03 08:18:51 -0500

    Add app_name prompt and validation for version and app_id

--- meta.js
+++ meta.js
@@ -1,3 +1,27 @@
+const validateAppId = (app_id) => {
+  let parts = app_id.split('.');
+  if (parts.length < 2) {
+    return 'App ID must contain two or more strings, separated by a dot.';
+  }
+  if (parts.some(part => {
+      return !/^[a-zA-Z]\w+$/i.test(part);
+    })) {
+    return 'Each string must start with a letter and should contain only letters and numbers.';
+  }
+  if (!/^[a-z]/.test(app_id[0])) {
+    return 'App ID must start with a lowercase letter.';
+  }
+
+  return true;
+};
+
+const validateVersion = (version) => {
+  if (!/^(\d+\.)(\d+\.)(\d)$/.test(version)) {
+    return 'Version must have major, minor and patch numbers.';
+  }
+  return true;
+};
+
 module.exports = {
   prompts: {
     name: {
@@ -5,22 +29,30 @@ module.exports = {
       required: true,
       label: 'Project name',
     },
-    app_id: {
-      type: 'string',
-      required: true,
-      label: 'Application ID',
-      default: 'org.nativescript.application',
-    },
     description: {
       type: 'string',
       label: 'Project description',
-      default: 'NativeScript-Vue application',
+      default: 'A native application built with NativeScript-Vue',
+    },
+    app_name: {
+      type: 'string',
+      required: true,
+      label: 'Application name',
+      default: 'NativeScript-Vue Application',
+    },
+    app_id: {
+      type: 'string',
+      required: true,
+      label: 'Unique application identifier',
+      default: 'org.nativescript.application',
+      validate: validateAppId,
     },
     version: {
       type: 'string',
       required: true,
       label: 'Project version',
       default: '1.0.0',
+      validate: validateVersion,
     },
     author: {
       type: 'string',
@@ -33,20 +65,17 @@ module.exports = {
     },
     router: {
       type: 'confirm',
-      label: 'Vue-Router',
+      label: 'Install vue-router?',
     },
     store: {
       type: 'confirm',
-      label: 'Vuex store',
+      label: 'Install vuex?',
     },
   },
   helpers: {
     androidVersionCode: (version) => {
       const parts = version.split('.');
       return parts[0] + '0' + parts[1] + '0' + parts[2];
-    },
-    cleanAppName: (name) => {
-      return name.replace(/\W/g, '');
     },
   },
   filters: {
--- template/README.md
+++ template/README.md
@@ -1,4 +1,4 @@
-# {{ name }}
+# {{ app_name }}
 
 > {{ description }}
 
