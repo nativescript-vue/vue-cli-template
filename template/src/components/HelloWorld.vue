2e2bdd7c3debf8b40151e6d2af1c91c5067007b0
David Genest <david.genest@gmail.com>
2018-03-14 21:24:10 -0400

    Label needs a text attribute to display correctly (#17)
    Starter HelloWorld.vue does not display the Label correctly
    which can be confusing for newcomers.
    
    Use correct syntax for the Label.

--- template/src/components/HelloWorld.vue
+++ template/src/components/HelloWorld.vue
@@ -7,9 +7,7 @@
     </ActionBar>
 
     <StackLayout class="hello-world">
-      <Label class="body">
-        This is a hello world component, tap the button if you dare
-      </Label>
+      <Label class="body" textWrap=true text="This is a hello world component, tap the button if you dare"/>
 
       <Button class="btn btn-primary" @tap="surprise = !surprise" text="Tap me!"/>
       <Image v-if="surprise" src="~/images/NativeScript-Vue.png"/>
