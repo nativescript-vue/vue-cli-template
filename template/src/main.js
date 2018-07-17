30b7dcd59d0744dcfa0c7bfeeb6d60c3188a4f33
Tim Martin <tmartin1@users.noreply.github.com>
2018-07-17 15:57:15 -0400

    Fix spelling error in main.js
    Just noticed a spelling error in the comment on line 19 in `template/src/main.js`. This just fixes that small typo.

--- template/src/main.js
+++ template/src/main.js
@@ -16,7 +16,7 @@ import store from './store';
 
 import './styles.scss';
 
-// Uncommment the following to see NativeScript-Vue output logs
+// Uncomment the following to see NativeScript-Vue output logs
 //Vue.config.silent = false;
 
 new Vue({
