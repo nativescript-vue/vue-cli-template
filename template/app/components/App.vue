8684da86e121a70b4de885278e7d272de6e7fdc9
Igor Randjelovic <rigor789@gmail.com>
2018-09-21 01:22:13 +0200

    feat: multiple presets (#91)

---
+++ template/app/components/App.vue
@@ -0,0 +1,146 @@
+{{#if_eq preset "Simple"}}<template>
+    <Page>
+        <ActionBar title="Welcome to NativeScript-Vue!"/>
+        <GridLayout colums="*" rows="*">
+            <Label class="message" :text="msg" col="0" row="0"/>
+        </GridLayout>
+    </Page>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        msg: 'Hello World!'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    ActionBar {
+        background-color: #53ba82;
+        color: #ffffff;
+    }
+
+    .message {
+        vertical-align: center;
+        text-align: center;
+        font-size: 20;
+        color: #333333;
+    }
+</style>{{/if_eq}}{{#if_eq preset "TabView"}}<template>
+    <Page>
+        <ActionBar title="Welcome to NativeScript-Vue!" android:flat="true"/>
+        <TabView android:tabBackgroundColor="#53ba82"
+                 android:tabTextColor="#c4ffdf"
+                 android:selectedTabTextColor="#ffffff"
+                 androidSelectedTabHighlightColor="#ffffff">
+            <TabViewItem title="Tab 1">
+                <GridLayout colums="*" rows="*">
+                    <Label class="message" :text="msg" col="0" row="0"/>
+                </GridLayout>
+            </TabViewItem>
+            <TabViewItem title="Tab 2">
+                <GridLayout colums="*" rows="*">
+                    <Label class="message" text="Tab 2 Content" col="0" row="0"/>
+                </GridLayout>
+            </TabViewItem>
+            <TabViewItem title="Tab 3">
+                <GridLayout colums="*" rows="*">
+                    <Label class="message" text="Tab 3 Content" col="0" row="0"/>
+                </GridLayout>
+            </TabViewItem>
+        </TabView>
+    </Page>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        msg: 'Hello World!'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    ActionBar {
+        background-color: #53ba82;
+        color: #ffffff;
+    }
+
+    .message {
+        vertical-align: center;
+        text-align: center;
+        font-size: 20;
+        color: #333333;
+    }
+</style>{{/if_eq}}{{#if_eq preset "SideDrawer"}}<template>
+    <Page>
+        <ActionBar>
+            <GridLayout width="100%" columns="auto, *">
+                <Label text="MENU" @tap="$refs.drawer.nativeView.showDrawer()" col="0"/>
+                <Label class="title" text="Welcome to NativeScript-Vue!"  col="1"/>
+            </GridLayout>
+        </ActionBar>
+
+        <RadSideDrawer ref="drawer">
+            <StackLayout ~drawerContent backgroundColor="#ffffff">
+                <Label class="drawer-header" text="Drawer"/>
+
+                <Label class="drawer-item" text="Item 1"/>
+                <Label class="drawer-item" text="Item 2"/>
+                <Label class="drawer-item" text="Item 3"/>
+            </StackLayout>
+
+            <GridLayout ~mainContent colums="*" rows="*">
+                <Label class="message" :text="msg" col="0" row="0"/>
+            </GridLayout>
+        </RadSideDrawer>
+    </Page>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        msg: 'Hello World!'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    ActionBar {
+        background-color: #53ba82;
+        color: #ffffff;
+    }
+
+    .title {
+        text-align: left;
+        padding-left: 16;
+    }
+
+    .message {
+        vertical-align: center;
+        text-align: center;
+        font-size: 20;
+        color: #333333;
+    }
+
+    .drawer-header {
+        padding: 50 16 16 16;
+        margin-bottom: 16;
+        background-color: #53ba82;
+        color: #ffffff;
+        font-size: 24;
+    }
+
+    .drawer-item {
+        padding: 8 16;
+        color: #333333;
+        font-size: 16;
+    }
+</style>{{/if_eq}}
