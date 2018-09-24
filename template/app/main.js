ef7c574498503b739dc5b135024bbfbc979ab826
DevPlus31 <b.mus@live.fr>
2018-09-24 23:02:55 +0100

    register RadSideDrawer to Vue

--- template/app/main.js
+++ template/app/main.js
@@ -9,6 +9,10 @@ if(TNS_ENV !== 'production') {
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = (TNS_ENV === 'production')
 
+{{#if_eq preset "SideDrawer"}}
+Vue.registerElement('RadSideDrawer', () => require('nativescript-ui-sidedrawer').RadSideDrawer)
+{{/if_eq}}
+
 new Vue({
   render: h => h('frame', [h(App)])
 }).$start()
