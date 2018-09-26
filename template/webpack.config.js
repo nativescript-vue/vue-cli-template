2f96525d636c13126e8f5395bc828ce6e70a2b19
Vasil Trifonov <v.trifonov@gmail.com>
2018-09-26 20:27:10 +0300

    Refactor css bundling

--- template/webpack.config.js
+++ template/webpack.config.js
@@ -3,7 +3,6 @@ const { relative, resolve, sep } = require("path");
 const webpack = require("webpack");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
@@ -47,6 +46,10 @@ module.exports = env => {
             hmr, // --env.hmr
     } = env;
 
+    const externals = (env.externals || []).map((e) => { // --env.externals
+        return new RegExp(e + ".*");
+    });
+
     const mode = production ? "production" : "development"
 
     const appFullPath = resolve(projectRoot, appPath);
@@ -59,6 +62,7 @@ module.exports = env => {
     const config = {
         mode: mode,
         context: appFullPath,
+        externals,
         watchOptions: {
             ignored: [
                 appResourcesFullPath,
@@ -127,9 +131,9 @@ module.exports = env => {
             minimize: Boolean(production),
             minimizer: [
                 new UglifyJsPlugin({
+                    parallel: true,
+                    cache: true,
                     uglifyOptions: {
-                        parallel: true,
-                        cache: true,
                         output: {
                             comments: false,
                         },
@@ -166,8 +170,7 @@ module.exports = env => {
                     test: /\.css$/,
                     use: [
                         'nativescript-dev-webpack/style-hot-loader',
-                        'css-hot-loader',
-                        MiniCssExtractPlugin.loader,
+                        'nativescript-dev-webpack/apply-css-loader.js',
                         { loader: "css-loader", options: { minimize: false, url: false } },
                     ],
                 },
@@ -175,8 +178,7 @@ module.exports = env => {
                     test: /\.scss$/,
                     use: [
                         'nativescript-dev-webpack/style-hot-loader',
-                        'css-hot-loader',
-                        MiniCssExtractPlugin.loader,
+                        'nativescript-dev-webpack/apply-css-loader.js',
                         { loader: "css-loader", options: { minimize: false, url: false } },
                         "sass-loader",
                     ],
@@ -196,9 +198,6 @@ module.exports = env => {
         },
         plugins: [
             // ... Vue Loader plugin omitted
-            new MiniCssExtractPlugin({
-                filename: `app.css`,
-            }),
             // make sure to include the plugin!
             new VueLoaderPlugin(),
             // Define useful constants like TNS_WEBPACK
