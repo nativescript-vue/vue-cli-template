9de50c6a0c126f74b7ebc2e9039a3a1c13f2dbd8
Manuel Saelices <msaelices@gmail.com>
2019-01-28 20:06:17 +0100

    feat (ts): More script to fix for TS

--- template/app/components/App.vue
+++ template/app/components/App.vue
@@ -7,7 +7,7 @@
     </Page>
 </template>
 
-<script>
+<script {{#if_eq lang "typescript" }}lang="ts"{{/if_eq}}>
   export default {
     data() {
       return {
@@ -102,7 +102,7 @@
     </Page>
 </template>
 
-<script>
+<script {{#if_eq lang "typescript" }}lang="ts"{{/if_eq}}>
   export default {
     data() {
       return {
