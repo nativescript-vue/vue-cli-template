7fe366ab544c5712ae95016c4806ccba9e62fc47
Manuel Saelices <msaelices@gmail.com>
2019-01-28 22:57:35 +0100

    feat (ts): Better TS format for the main.ts file

--- template/app/main.ts
+++ template/app/main.ts
@@ -1,18 +1,21 @@
-import Vue from 'nativescript-vue'
-import App from './components/App.vue'
-{{#devtools}}import VueDevtools from 'nativescript-vue-devtools'
+import Vue from 'nativescript-vue';
+import App from './components/App.vue';
+{{#devtools}}import VueDevtools from 'nativescript-vue-devtools';
 
 if(TNS_ENV !== 'production') {
-  Vue.use(VueDevtools)
+  Vue.use(VueDevtools);
 }
 {{/devtools}}
 // Prints Vue logs when --env.production is *NOT* set while building
-Vue.config.silent = (TNS_ENV === 'production')
+Vue.config.silent = (TNS_ENV === 'production');
 
 {{#if_eq preset "SideDrawer"}}
-Vue.registerElement('RadSideDrawer', () => require('nativescript-ui-sidedrawer').RadSideDrawer)
+Vue.registerElement(
+  'RadSideDrawer',
+  () => require('nativescript-ui-sidedrawer').RadSideDrawer
+)
 {{/if_eq}}
 
 new Vue({
   render: h => h('frame', [h(App)])
-}).$start()
+}).$start();
