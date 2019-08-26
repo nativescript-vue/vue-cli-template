a4ba46830107f0e374d28d0083d635b315ceef52
Igor Randjelovic <rigor789@gmail.com>
2019-08-26 18:35:35 +0200

    style: reformat code

--- template/app/main.js
+++ template/app/main.js
@@ -1,18 +1,22 @@
 import Vue from 'nativescript-vue'
 import App from './components/App'
-{{#store}}import store from './store';{{/store}}
 
 {{#devtools}}import VueDevtools from 'nativescript-vue-devtools'
+{{#store}}import store from './store'{{/store}}
 
 if(TNS_ENV !== 'production') {
   Vue.use(VueDevtools)
 }
 {{/devtools}}
+  
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = (TNS_ENV === 'production')
 
 {{#if_eq preset "SideDrawer"}}
-Vue.registerElement('RadSideDrawer', () => require('nativescript-ui-sidedrawer').RadSideDrawer)
+Vue.registerElement(
+  'RadSideDrawer',
+  () => require('nativescript-ui-sidedrawer').RadSideDrawer
+)
 {{/if_eq}}
 
 new Vue({
