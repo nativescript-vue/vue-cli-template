6a015cb215e7d3213bd545179819edcc2ae1f13c
Igor Randjelovic <rigor789@gmail.com>
2020-09-13 12:50:06 +0200

    fix: ts template

--- template/nativescript.config.ts
+++ template/nativescript.config.ts
@@ -2,6 +2,7 @@ import { NativeScriptConfig } from '@nativescript/core'
 
 export default {
   id: '{{ app_id }}',
+  appPath: 'src',
   appResourcesPath: 'App_Resources',
   android: {
     v8Flags: '--expose_gc',
